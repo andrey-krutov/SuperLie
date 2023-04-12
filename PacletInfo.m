5c9a420e0a755340bef4bb9ef1228ff1e19fd88b
Andrey Krutov <a.o.krutov@gmail.com>
2023-04-12 15:52:11 +0200

    PacletInfo.m created

---
+++ PacletInfo.m
@@ -0,0 +1,13 @@
+(* Paclet Info File *)
+
+(* created 2023/04/12*)
+
+Paclet[
+    Name -> "SuperLie",
+    Version -> "2.08",
+    WolframVersion -> "11+",
+    Description -> "A package for Lie Algebra and Super Algebra computations in Mathematica software system\n\nOriginally developed by Pavel Grozman\nCurrently maintained by Andrey Krutov",
+    Creator -> "Pavel Grozman"
+]
+
+
