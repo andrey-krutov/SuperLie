7fb042eb44b2d3a7aca70c79ea68ccd3b9f95b4b
Andrey Krutov <a.o.krutov@gmail.com>
2024-04-19 17:40:43 +0200

    bump the version

--- SuperLie/SuperLie.m
+++ SuperLie/SuperLie.m
@@ -2368,7 +2368,7 @@ CreatePalette[{Cell[BoxData[
   Method->"Preemptive"]], NotebookDefault]},WindowTitle->"SuperLie"]]
 *)
 
-Print["SuperLie Package Version 2.08 Beta 07 AK03 installed\nDisclaimer: This software is provided \"AS IS\", without a warranty of any kind"]
+Print["SuperLie Package Version 2.08 Beta 07 AK04 installed\nDisclaimer: This software is provided \"AS IS\", without a warranty of any kind"]
 
 EndPackage[]
 
